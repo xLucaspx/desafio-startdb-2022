e63f8ed2ba34099af7a4798590eb3f6fe53d0112
xLucaspx <xlucaspx@gmail.com>
2022-07-20 18:15:44 -0300

    Adicionando construtor da classe Forca

--- desafio/forca.js
+++ desafio/forca.js
@@ -1,5 +1,14 @@
 class Forca {
 
+  constructor(palavra) {
+    this.palavraSecreta = palavra.toLowerCase(); //para garantir que o jogo não será case sensitive
+    this.caracteres = this.palavraSecreta.split("");
+    this.acertos = [];
+    this.letrasChutadas = [];
+    this.vidas = 6;
+    this.palavra = this.mostraTracos(this.palavraSecreta); //array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
+  }
+
   chutar(letra) { }
 
   buscarEstado() { return ""; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
